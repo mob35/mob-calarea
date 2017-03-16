e1a6a90c45a671bec2c1e4f69bdbb5c043d8108f
วัจน์กร จงกลาง < >
2017-03-16 14:54:26 +0700

    updayr -v

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var exports = module.exports = {};
 exports.sq = function (width, height) {
   return width * height;
 };
+
 exports.tri = function (width, height) {
   return (width * height) * 0.5;
-};
+}; 
