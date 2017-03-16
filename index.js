d747eef1ed55498da7125334a2cbb85400797420
วัจน์กร จงกลาง < >
2017-03-16 14:53:45 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = function(width, height) {
+var exports = module.exports = {};
+
+exports.sq = function (width, height) {
   return width * height;
+};
+exports.tri = function (width, height) {
+  return (width * height) * 0.5;
 };
